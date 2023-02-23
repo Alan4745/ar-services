c7e02a727487ee64e12c04a52a1bfcf031d88ad1
Alan Eduardo De La Cruz Najera <alaneduardodelacruz407@gmail.com>
2023-02-23 10:57:46 -0600

    test float

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Suspense, useState } from 'react';
 import {
-  ZapparCamera, InstantTracker, ZapparCanvas, BrowserCompatibility, Loader , 
+  ZapparCamera, InstantTracker, ZapparCanvas, BrowserCompatibility, Loader,
 } from '@zappar/zappar-react-three-fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { useLoader } from '@react-three/fiber'
+import { Float } from '@react-three/drei';
 
 function App() {
   const [placementMode, setPlacementMode] = useState(true);
+  // const gltf = useLoader(GLTFLoader, './assets/63e6a9496f759e4d1df93573.glb')
+
+  // console.log(gltf.scene)
   return (
     <>
       <BrowserCompatibility />
       <ZapparCanvas>
         <ZapparCamera />
         <InstantTracker placementMode={placementMode} placementCameraOffset={[0, 0, -5]}>
-        <Suspense fallback={null}>
-          <mesh>
-            <boxBufferGeometry />
-            <meshStandardMaterial color="hotpink" />
-          </mesh>
-          </Suspense> 
+          <Suspense fallback={null}>
+            <Float
+              speed={1.4} // Animation speed, defaults to 1
+              rotationIntensity={1} // XYZ rotation intensity, defaults to 1
+              floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+            >
+              <mesh>
+                <tetrahedronGeometry />
+                <meshStandardMaterial color="hotpink" />
+              </mesh>
+            </Float>
+          </Suspense>
         </InstantTracker>
         <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
-      <Loader />
+        <Loader />
       </ZapparCanvas>
       <div
         id="zappar-button"
