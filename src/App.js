6954fa221149daa9384c0b619d2d036293fd2c0c
Alan Eduardo De La Cruz Najera <alaneduardodelacruz407@gmail.com>
2023-02-23 12:25:51 -0600

    test obj animation

--- src/App.js
+++ src/App.js
@@ -2,49 +2,52 @@ import React, { Suspense, useState } from 'react';
 import {
   ZapparCamera, InstantTracker, ZapparCanvas, BrowserCompatibility, Loader,
 } from '@zappar/zappar-react-three-fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { useLoader } from '@react-three/fiber'
 import { Float } from '@react-three/drei';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { useFrame, useLoader } from '@react-three/fiber';
+import * as THREE from 'three'
+import glb from './assets/waving.glb'
 
+const Model = (props) => {
+  const clock = new THREE.Clock()
+  const gltf = useLoader(GLTFLoader, glb);
+  const mixer = new THREE.AnimationMixer(gltf.scene)
 
-// const Model = () => {
-//   const clock = new THREE.Clock()
-//   const gltf = useLoader(GLTFLoader, './assets/waving.glb')
+  var action = mixer.clipAction(gltf.animations[0])
+  action.play()
 
-// };
+  useFrame(() => mixer.update(clock.getDelta()))
+
+  // console.log(gltf.animations)
+  return <primitive object={gltf.scene} />
+}
 
 function App() {
   const [placementMode, setPlacementMode] = useState(true);
+  const [test, setTest] = useState(false);
+  console.log(glb)
 
   return (
     <>
       <BrowserCompatibility />
       <ZapparCanvas>
         <ZapparCamera />
-        <InstantTracker placementMode={placementMode} placementCameraOffset={[0, 0, -5]}>
-          <Suspense fallback={null}>
+        <Suspense fallback={null}>
+          <InstantTracker placementMode={placementMode} placementCameraOffset={[0, 0, -5]}>
             <Float
               speed={1.4} // Animation speed, defaults to 1
               rotationIntensity={1} // XYZ rotation intensity, defaults to 1
               floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
             >
-              <mesh position={[0.90, 0, -0.50]}>
+              {/* <mesh>
                 <tetrahedronGeometry />
                 <meshStandardMaterial color="hotpink" />
-              </mesh>
+              </mesh> */}
+              <Model prueba={test} />
+              {/* <primitive object={gltf.scene}/> */}
             </Float>
-            <Float
-              speed={2} // Animation speed, defaults to 1
-              rotationIntensity={1} // XYZ rotation intensity, defaults to 1
-              floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
-            >
-              <mesh position={[-0.90, 0, -0.50]}>
-                <tetrahedronGeometry />
-                <meshStandardMaterial color="hotpink" />
-              </mesh>
-            </Float>
-          </Suspense>
-        </InstantTracker>
+          </InstantTracker>
+        </Suspense>
         <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
         <Loader />
       </ZapparCanvas>
