fa75059cc62cf590f57c4dc9fb78af929a3f482e
Eduardo-45 <115485379+Alan4745@users.noreply.github.com>
2023-02-27 15:39:20 -0600

    test fetch url
    Error in HTTP petition headers from Firebase Store

--- src/App.js
+++ src/App.js
@@ -1,139 +1,55 @@
 import React, { Suspense, useState } from 'react';
-import {
-  ZapparCamera, InstantTracker, ZapparCanvas, BrowserCompatibility, Loader,
-} from '@zappar/zappar-react-three-fiber';
-import { Float } from '@react-three/drei';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { useFrame, useLoader } from '@react-three/fiber';
-import * as THREE from 'three'
-import glb from './assets/waving.glb'
-import gbl1 from './assets/old_rusty_car.glb'
-import gbl2 from './assets/male_person.glb'
-import gbl3 from './assets/the_big_g_building.glb'
-import gbl4 from './assets/cyberpunk_card.glb'
+import { deleteFile, uploadFile } from './firebase/config'
+import Ar from './screen/Ar';
+import Model from './screen/Model';
 
-const Model = (props) => {
-  // const clock = new THREE.Clock()
-  console.log(props.model)
-  const gltf = useLoader(GLTFLoader, props.model);
-  // const mixer = new THREE.AnimationMixer(gltf.scene)
-
-  // var action = mixer.clipAction(gltf.animations[0])
-  // action.play()
-
-  // useFrame(() => mixer.update(clock.getDelta()))
-
-  // console.log(gltf.animations)
-  console.log(props.size)
-  return <primitive object={gltf.scene} scale={props.size} position={props.posicion} rotation={[0, 0 * Math.PI / 180, 0]} />
-}
 
 function App() {
-  // const gltf = useLoader(GLTFLoader, data);
-  const [data, setData] = useState(glb);
+  const [file, setFile] = useState(null);
+  const [urlfile, setUrlFile] = useState(null);
+  const [path, setPath] = useState(null);
+  const [modelCargado, setModelCargado] = useState(false)
 
-  const [placementMode, setPlacementMode] = useState(true);
-  const [test, setTest] = useState(false);
-  console.log(data)
+  const handleSumit = async (e) => {
+    e.preventDefault()
+    console.log('subiendo archivo')
+    // const result = await uploadFile(file)
+    // console.log(result)
 
-  // var url = 'https://api.sketchfab.com/v3/models/90d980fb471f440aa1d16866c79c61f7/download';
-  // var options = {
-  //   method: 'GET',
-  //   headers: {
-  //     Authorization: `Bearer ${'zljrX7JwA8Qhcz3aUpfYpqj8XHgLW7'}`,
-  //   },
-  //   mode: 'cors'
-  // };
-
-  // fetch(url, options).then(function (response) {
-  //   return response.json();
-  // }).then(function (data) {
-  //   console.log(data);
-  // });
-
-
-  const [value, setValue] = useState(1); // Valor inicial del slider
-  const [ejeY, setEjeY] = useState([0, 0.50, 0]);
-
-
-
-
-  const handleChange = (event) => {
-    setValue(event.target.value); // Actualizar el valor del slider
-  };
-
-  const handleChange1 = (event) => {
-    // setEjeY(event.target.value); // Actualizar el valor del slider
-    const newArray = [...ejeY];
-    newArray[1] = event.target.value;
-    setEjeY(newArray);
-
-    console.log(newArray)
-  };
+    try {
+      const result = await uploadFile(file);
+      console.log(result.url);
+      console.log(result.metaData.fullPath);
+      setUrlFile(result.url);
+      setPath(result.metaData.fullPath);
+      setModelCargado(true)
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
-    <>
-      <BrowserCompatibility />
-      <ZapparCanvas>
-        <ZapparCamera />
-        <Suspense fallback={null}>
-          <InstantTracker placementMode={placementMode} placementCameraOffset={[0, 0, -5]}>
-            <Float
-              speed={1.4} // Animation speed, defaults to 1
-              rotationIntensity={1} // XYZ rotation intensity, defaults to 1
-              floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
-            >
-              {/* <mesh>
-                <tetrahedronGeometry />
-                <meshStandardMaterial color="hotpink" />
-              </mesh> */}
-              <Model prueba={test} model={data} size={value} posicion={ejeY} />
-              {/* return <primitive object={gltf.scene} scale={0.1} rotation={[0, 75 * Math.PI / 180, 0]} /> */}
-
-              {/* <primitive object={gltf.scene}/> */}
-            </Float>
-          </InstantTracker>
-        </Suspense>
-        <directionalLight position={[1, 1, 5]} intensity={2} />
-        <Loader />
-      </ZapparCanvas>
-      <div
-        id="zappar-button"
-        role="button"
-        onKeyPress={() => { setPlacementMode(((currentPlacementMode) => !currentPlacementMode)); }}
-        tabIndex={0}
-        onClick={() => { setPlacementMode(((currentPlacementMode) => !currentPlacementMode)); }}
-      >
-        Tap here to
-        {placementMode ? ' place ' : ' pick up '}
-        the object
-      </div>
-      <div id="buton-change"
-        role='button'
-        tabIndex={0}
-      // onClick={() => { data !== 'gbl4' ? setData(gbl4) : setData(gbl3) }}
-      >
-        <button style={{ width: '50%' }} onClick={() => { setData(gbl1)}}>
-          model 1
-        </button>
-        <button style={{ width: '50%' }} onClick={() => { setData(gbl2) }}>
-          model 2
-        </button>
-        <button style={{ width: '50%' }} onClick={() => { setData(gbl3) }}>
-          model 3
-        </button>
-        <button style={{ width: '50%' }} onClick={() => { setData(gbl4) }}>
-          model 4
-        </button>
-
-      </div>
-      <div id="change_size">
-        <input type="range" min="0" max="5" step="0.01" value={value} onChange={handleChange} />
-      </div>
-      <div id="change_posicion">
-        <input type="range" min="-5" max="5" step="0.01" value={ejeY[1]} onChange={handleChange1} />
-      </div>
-    </>
+    <div>
+      {
+        !modelCargado ?
+          <div>
+            <h1>Estamos en la pantalla de inicio</h1>
+            <form onSubmit={handleSumit}>
+              <input
+                type="file"
+                name=''
+                id=''
+                onChange={e => setFile(e.target.files[0])} />
+              <button>upload</button>
+            </form>
+            <button onClick={() => { deleteFile('4d0bf7e3-7314-42c0-8e16-83c918d656b4.glb') }}>Delete</button>
+          </div>
+          :
+          <>
+          <Ar url={urlfile}/>
+          </>
+      }
+    </div>
   );
 }
 
---
+++ src/screen/Model.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { useFrame, useLoader } from '@react-three/fiber';
+import * as THREE from 'three'
+
+
+export default function Model(props) {
+    console.log(props.model)
+    const gltf = useLoader(GLTFLoader, 'https://firebasestorage.googleapis.com/v0/b/lndmrk-64e90.appspot.com/o/3D%2F645a074a-10e3-45e0-844a-e148f3a44acc.glb?alt=media&token=de935f91-1657-4e86-b5be-2c4c886a6be7');
+    // const mixer = new THREE.AnimationMixer(gltf.scene)
+  
+    // var action = mixer.clipAction(gltf.animations[0])
+    // action.play()
+  
+    // useFrame(() => mixer.update(clock.getDelta()))
+  
+    // console.log(gltf.animations)
+    console.log(props.size)
+
+  return (
+    <primitive object={gltf.scene} scale={props.size} position={props.posicion} rotation={[0, 0 * Math.PI / 180, 0]} />
+    )
+}
